e0cf67d59aabba64cb9b458bf15e3551eb05d041
Ansh Balde <anshbalde@gmail.com>
2018-03-08 09:25:38 +0530

    Added About Component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./About.css";
+
+class About extends Component {
+  render() {
+    return (
+      <div className="about">
+        <h1>
+          Will be updated soon
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default About;
