7e1a18beb3b989d7fe64d6611c19cba865e38799
Ansh Abhay Balde <31566246+ansh103@users.noreply.github.com>
2018-03-17 23:32:48 +0530

    Add Favicon and File

---
+++ src/html.jsx
@@ -0,0 +1,66 @@
+/* eslint import/no-unresolved:"off" */
+/* eslint import/extensions:"off" */
+/* eslint global-require:"off" */
+import React from "react";
+import favicon from "./favicon.png";
+
+let inlinedStyles = "";
+if (process.env.NODE_ENV === "production") {
+  try {
+    /* eslint import/no-webpack-loader-syntax: off */
+    inlinedStyles = require("!raw-loader!../public/styles.css");
+  } catch (e) {
+    /* eslint no-console: "off" */
+    console.log(e);
+  }
+}
+
+export default class HTML extends React.Component {
+  render() {
+    let css;
+    if (process.env.NODE_ENV === "production") {
+      css = (
+        <style
+          id="gatsby-inlined-css"
+          dangerouslySetInnerHTML={{ __html: inlinedStyles }}
+        />
+      );
+    }
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+
+          {/* Mobile Meta */}
+          <meta name="HandheldFriendly" content="True" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+
+          {/* Styles'n'Scripts */}
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="//fonts.googleapis.com/css?family=Merriweather:300,700,700italic,300italic|Open+Sans:700,400"
+          />
+
+          {this.props.headComponents}
+          <link rel="shortcut icon" href={favicon} />
+          {css}
+        </head>
+        <body>
+          <div
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    );
+  }
+}
